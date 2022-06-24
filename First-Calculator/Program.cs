7be2de6d73e80cfedce681b145c95cefe3175c7b
elijah amoako <elijahamoako72@gmail.com>
2022-06-24 20:52:17 +0000

    FirstCalculator

---
+++ First-Calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+
+
+
+Console.Write("Enter a number:  ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Enter an operator:  ");
+string op = Console.ReadLine();
+
+
+Console.Write("Enter a number:  ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+if (op == "+")
+{
+    Console.WriteLine($"The answer is = {num1+num2}");
+}
+else if (op == "-")
+{
+    Console.WriteLine($"The answer is = {num1 - num2}");
+}
+else if (op == "x")
+{
+    Console.WriteLine($"The answer is = {num1 * num2}");
+}
+else if (op == "/")
+{
+    Console.WriteLine($"The answer is = {num1 / num2}");
+}
+else
+{
+    Console.WriteLine($"You entered an invalid operator");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
